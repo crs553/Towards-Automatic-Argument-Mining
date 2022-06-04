70ebc277490e9a666dcf4a8153fd860b41b1e9c6
charlie@popOS <crs553@york.ac.uk>
2022-06-04 22:34:50 +0100

    Implementing loading of data

--- dataReader/dataReader.py
+++ dataReader/dataReader.py
@@ -16,33 +16,25 @@ class Reader():
 
         self.__path = path
         print("Loading files")
-        self.filenames = [i for i in glob(self.__path + "/brat-project-final/*.txt")]
+        self.filenames = [i for i in glob(self.__path + "/brat-project-final/*.xml")]
+        self.directory = self.filenames[0][:-16]
         print(f"Detected {len(self.filenames)} files:")
+        print(self.filenames[0])
         self.filenames.sort()
         for num, i in enumerate(self.filenames):
             t = "\n" if num % 10 == 0 else ""
-            print(f"{i[-12:]}\t", end= t)
+            print(f"{i[-16:]}\t", end=t)
         print()
-        self.__test_train_split = self.__setup_train_test()
-        self.__anns = self.__load_annotations()
 
     @property
     def path(self) -> str:
         return self.__path
 
     @property
-    def test_train_list(self) -> list:
-        return self.__test_train_split
+    def len_dataset(self) -> int:
+        return len(self.filenames)
 
-    @property
-    def annotations(self) -> list:
-        return self.__anns
-
-    @property
-    def texts(self) -> list:
-        return self.__texts
-
-    def __setup_train_test(self) -> list:
+    def setup_train_test(self) -> list:
         """
         Finds train and test type for each file splits and returns them as list
         :return:
@@ -59,43 +51,270 @@ class Reader():
 
         return splits
 
-    def open_text(self, file_id) -> (str,str):
-        """Open all text files within brat-project-final
-        :param file_id: index of the file to load
-        :return: tuple of filename and the file contents"""
-        local_text = None
+    def load_from_directory(self, rst_files=False, ADU=False):
+        """Load all files in the directory, creates relation matrix for them
+        Input:
+            directory: directory with annotation files
+            rst_file: True, if the directory stores RST files as well
+            ADU: True for proposition type data loading
+        Output:
+            dataFrame: pandas DataFrame with samples as rows"""
 
-        fname = self.filenames[file_id]
-        f = open(fname, "r")
-        file = f.read()
-        f.close()
-        local_text = (fname[-12:], file)
+        data_list = list()
 
-        if not local_text:
-            raise ValueError("Could not load file")
+        for (e, annotation_file) in enumerate(self.filenames):
+            if annotation_file[-7:] not in ['ann.xml']:
+                continue
+            if not ADU:
+                file_data = self.load_single_file(e, rst_files)
+            else:
+                file_data = self.load_for_ADU_types(e)
+            data_list = data_list + file_data
+        dataFrame = pd.DataFrame.from_dict(data_list, orient='columns')
+        print('Loaded data length: ' + str(len(dataFrame)))
+        return dataFrame
 
-        return local_text
-
-    def __load_annotations(self) -> list:
+    def load_single_file(self, fileID, rst=False) -> dict:
         """
-        Opens all annotation files within brat-project-final and creates a relational matrix
-        :return:  list of found annotation xml type files
+        Load a single file, creates relation matrix
+        :param fileID: index file
+        :param rst: True if RST files are stored and used
+        :return file_data: dictionary with following features:
+                arg1, arg2, argumentationID, label,
+                originalArg1, originalArg2, fullText1,
+                rstCon, rstConParent - only if RST active,
+                positionDiff, positArg1, positArg2,
+                sentenceDiff, sen1, sen2 - only if full text exists
         """
-        print("Directory annotation loading...")
 
+        file_data = []
+        relations = {}
 
-        anns = []
+        data = self.__get_single_data(fileID)
 
-        for i, fname in enumerate(self.filenames):
-            print(fname[-16:])
-            f = open(fname, "r")
-            file = f.read()
-            f.close()
-            print(fname[-16:])
-            print("\n\n"+file)
-            anns.append((fname[-16:], xmltodict.parse(file)))
+        xmlData = xmltodict.parse(data)
 
-        return anns
+        # if rst_files:
+        #     (recovered_string, prop_edu_dict) = load_merge(file_path)
+        #     edges = load_brackets(file_path)
+
+        argID = fileID
+
+        matrixLength = len(xmlData['Annotation']['Proposition'])
+        relationCount = 0
+        totalRelation = matrixLength * matrixLength
+        relationMatrix = (matrixLength, matrixLength)
+        relationMatrix = np.zeros(relationMatrix)
+        sent_tokenize_list = None
+
+        original_text = None
+        original_text2 = None
+        sens = None
+
+        propositions = xmlData['Annotation']['Proposition']
+        if 'OriginalText' in xmlData['Annotation']:
+            original_text = xmlData['Annotation']['OriginalText']
+            original_text2 = original_text.replace('\n', ' ')
+            sent_tokenize_list = sent_tokenize(original_text)
+            sens = len(sent_tokenize_list)
+
+        for prop_id in range(len(propositions)):
+            currentProposition = propositions[prop_id]
+
+            if currentProposition['ADU']['@type'] != 'conclusion' and 'Relation' in currentProposition.keys():
+
+                partners = list()
+                relationTypeList = list()
+
+                if currentProposition['Relation'].__class__ == list().__class__:
+                    for relation in range(len(currentProposition['Relation'])):
+                        relation_data = currentProposition['Relation'][relation]
+
+                        partners.append(relation_data['@partnerID'])
+                        relationTypeList.append(relation_data['@typeBinary'])
+                else:
+
+                    relation_data = currentProposition['Relation']
+                    partners.append(relation_data['@partnerID'])
+                    relationTypeList.append(relation_data['@typeBinary'])
+
+                for partner_id in range(len(partners)):
+                    for prop_id2 in range(len(propositions)):
+                        if partners[partner_id] == propositions[prop_id2]['@id']:
+                            if relationTypeList[partner_id] == '0':
+                                relationMatrix[prop_id][prop_id2] = 1
+                                relationMatrix[prop_id2][prop_id] = -1
+                            elif relationTypeList[partner_id] == '1':
+
+                                relationMatrix[prop_id][prop_id2] = 2
+                                relationMatrix[prop_id2][prop_id] = -2
+                            else:
+                                relationMatrix[prop_id][prop_id2] = -3
+
+        for i in range(len(relationMatrix)):
+            for j in range(len(relationMatrix[i])):
+                if i != j and relationMatrix[i][j] > -3:
+                    proposition1 = propositions[i]['text']
+                    proposition2 = propositions[j]['text']
+                    if self.fit_tokenize_length_threshold(proposition1) \
+                            or self.fit_tokenize_length_threshold(proposition2):
+                        continue
+
+                    originalSentenceArg1 = propositions[i]['text']
+                    originalSentenceArg2 = propositions[j]['text']
+
+                    if 'TextPosition' in propositions[i].keys():
+                        if propositions[i]['TextPosition']['@start'] \
+                                != '-1' or propositions[j]['TextPosition'
+                        ]['@start'] != '-1':
+
+                            if propositions[i]['TextPosition']['@start'] \
+                                    != '-1':
+                                for sentence in sent_tokenize_list:
+
+                                    if propositions[i]['text'] in sentence:
+                                        originalSentenceArg1 = sentence
+                                        sen1 = sent_tokenize_list.index(sentence)
+
+                            if propositions[j]['TextPosition']['@start'] != '-1':
+
+                                for sentence in sent_tokenize_list:
+                                    if propositions[j]['text'] in sentence:
+                                        originalSentenceArg2 = sentence
+                                        sen2 = sent_tokenize_list.index(sentence)
+
+                    line_data = {
+                        'argumentationID': argID,
+                        'arg1': propositions[i]['text'],
+                        'originalArg1': originalSentenceArg1,
+                        'arg2': propositions[j]['text'],
+                        'originalArg2': originalSentenceArg2,
+                        'label': relationMatrix[i][j],
+                        'fullText1': original_text2,
+                    }
+
+                    # if rst_files:
+                    #     arg1_range = get_edus(propositions[i]['text'],
+                    #                           recovered_string, prop_edu_dict)
+                    #     arg2_range = get_edus(propositions[j]['text'],
+                    #                           recovered_string, prop_edu_dict)
+                    #     arg1_rsts = get_rst_stats(arg1_range, edges)
+                    #     arg2_rsts = get_rst_stats(arg2_range, edges)
+                    #     cn1 = arg1_rsts['connected_nodes']
+                    #     cn2 = arg2_rsts['connected_nodes']
+                    #     conn = False
+                    #     conn_parent = any([z in cn1 for z in cn2])
+                    #     for c in cn1:
+                    #         if c in arg2_range:
+                    #             conn = True
+                    #     for c in cn2:
+                    #         if c in arg1_range:
+                    #             conn = True
+                    #     line_data['rstCon'] = (1 if conn else 0)
+                    #     line_data['rstConParent'] = \
+                    #         (1 if conn_parent else 0)
+                    #
+                    # #                    line_data['posEduArg1'] = arg1_range[0]
+                    # #                    line_data['posEduArg2'] = arg2_range[0]
+
+                    positArg1 = int(propositions[i]['TextPosition']['@start'])
+                    positArg2 = int(propositions[j]['TextPosition']['@start'])
+                    if positArg1 != -1 and positArg2 != -1:
+                        posit = abs((positArg1 - positArg2) / len(original_text))
+                        line_data['positionDiff'] = posit
+                        line_data['positArg1'] = positArg1 / len(original_text)
+                        line_data['positArg2'] = positArg2 / len(original_text)
+                        senit = abs(sen1 - sen2)
+                        line_data['sentenceDiff'] = senit / sens
+                        line_data['sen1'] = sen1 / sens
+                        line_data['sen2'] = sen2 / sens
+
+                    file_data.append(line_data)
+        return file_data
+
+    def load_for_ADU_types(self, fileID):
+        """Loads ADU type features.
+        Input:
+            fileID - index for the processed files
+        Output:
+            file_data: dictionary with the features stored:
+                       arg1, argumentationID, label,
+                       originalArg1, fullText1, positArg1
+        """
+        file_data = list()
+        relationMatrix = {}
+        data = self.__get_single_data(fileID)
+
+        argID = fileID
+        xmlData = xmltodict.parse(data)
+
+        matrixLength = len(xmlData['Annotation']['Proposition'])
+        relationCount = 0
+        totalRelation = matrixLength * matrixLength
+        relationMatrix = (matrixLength, matrixLength)
+        relationMatrix = np.zeros(relationMatrix)
+        original_text2 = " "
+
+        xmlData = xmltodict.parse(data)
+
+        propositions = xmlData['Annotation']['Proposition']
+        if 'OriginalText' in xmlData['Annotation']:
+            original_text = xmlData['Annotation']['OriginalText']
+            original_text2 = original_text.replace('\n', ' ')
+            sent_tokenize_list = sent_tokenize(original_text)
+            sens = len(sent_tokenize_list)
+
+        for prop_id in range(len(propositions)):
+            currentProposition = propositions[prop_id]
+
+            if currentProposition['ADU']['@type'] == 'conclusion':
+                aduType = 2
+            elif currentProposition['ADU']['@type'] == 'claim':
+                aduType = 1
+            elif currentProposition['ADU']['@type'] == 'premise':
+                aduType = 0
+            else:
+                err_ADU = currentProposition['ADU']['@type']
+                raise ValueError('Unexpected ADU type: ' + err_ADU)
+
+            arg1 = currentProposition['text']
+            originalSentenceArg1 = arg1
+            positArg1 = -1
+
+            if currentProposition['TextPosition']['@start'] != '-1':
+                for sentence in sent_tokenize_list:
+
+                    if arg1 in sentence:
+                        originalSentenceArg1 = sentence
+                        sen1 = sent_tokenize_list.index(sentence)
+
+                positArg1 = int(currentProposition['TextPosition']['@start'])
+            line_data = {
+                'argumentationID': argID,
+                'arg1': arg1,
+                'originalArg1': originalSentenceArg1,
+                'label': aduType,
+                'fullText1': original_text2,
+                'positArg1': positArg1 / len(original_text2),
+            }
+            file_data.append(line_data)
+        return file_data
+
+    def fit_tokenize_length_threshold(self,proposition, min_len=1, max_len=30):
+        """Drop out too long tokens"""
+
+        if len(sent_tokenize(proposition)) > min_len:
+            return True
+        elif len(word_tokenize(proposition)) > max_len:
+            return True
+        else:
+            return False
+
+    def __get_single_data(self, fileID):
+        with open(self.filenames[fileID], 'r') as file:
+            data = file.read()
+
+        return data
 
     def load_adu(self, fileid):
         """
@@ -182,6 +401,12 @@ if __name__ == "__main__":
     path = getcwd()[:-11] + "/ArgumentAnnotatedEssays-2.0"
     # path = "/home/charlie/Documents/Project/ArgumentAnnotatedEssays-2.0/"
     reader = Reader(path)
+
+    print()
+    t = reader.load_from_directory()
+    print()
+    print(t)
+
 
     # for a,b in splits:
     #     print(f"{a},{b}")
