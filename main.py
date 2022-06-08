f08c66bbaad500d4d47f8293d4332456a44bca91
charlie-desktop <crs553@york.ac.uk>
2022-06-08 21:05:54 +0100

    Editted to allow use of DI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import getcwd
+from Models import discourseIndicators
 
 
 # sorting out what method to take
@@ -7,18 +8,18 @@ def main(path: str) -> None:
     Start of Implementation of methods
     :param path_to_file:
     """
-    print("Please enter method type:\n> TS - Topic Similarity\n> SVM\n> TBD\n> CM - Combined Method (uses all above)")
+    print("Please enter method type:\n> DI - Discourse Indicators\n> SVM\n> TBD\n> CM - Combined Method (uses all above)")
     method_type = input().lower()
 
     # see if value appropriate
-    allowed = {"ts", "svm", "tbd", "cm"}
+    allowed = {"di", "svm", "tbd", "cm"}
     if method_type not in allowed:
         raise ValueError(f"{method_type} is not a correct value",
                          "Please restart the program and enter a correct value")
     # load in the data
 
-    if method_type == "ts":
-        pass
+    if method_type == "di":
+        discourseIndicators.main()
     elif method_type == "svm":
         pass
     elif method_type == "tbd":
